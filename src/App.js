4119b353b5f5643766ecb9b63ea8edfad240c036
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-11-06 19:47:06 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import LogIn from "./Pages/LogIn";
-import SignUp from "./Pages/SignUp";
+
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element = {<Home></Home>}/>
-        <Route path="/login" element = {<LogIn></LogIn>}/>
-        <Route path="/signup" element = {<SignUp></SignUp>}/>
-      </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      
+    </div>
   );
 }
 
