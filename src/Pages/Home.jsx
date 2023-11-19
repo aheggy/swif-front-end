94142c9de55f4bc6117acf9faeecae1874a7e9c3
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-11-19 12:37:10 -0500

    fix the home page login functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,56 +1,77 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode'; 
 import './Home.css';
 
-function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const API = process.env.REACT_APP_API_URL
 
-  const handleSubmit = (e) => {
+function Home() {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    
-    if (email.trim() === '' || password.trim() === '') {
-      alert('Please fill in all fields.');
-    } else {
-      setIsLoggedIn(true);
-      setIsSubmitted(true);
+    try {
+      const response = await fetch(`${API}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+
+        // Decode token to get the username
+        const decodedToken = jwtDecode(data.token);
+        const username = decodedToken.username; // Ensure token has username
+        window.location.href=`/${username}`; // Navigate to user's page
+      } else {
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.error('Error', error);
     }
   };
 
   return (
     <div className="container">
       <h1>Welcome to SWIF!</h1>
-      {isLoggedIn ? (
-        <div>
-          <p>Welcome back, {email}!</p>
-        </div>
-      ) : (
-        isSubmitted ? (
-          <p>Invalid email or password. Please try again.</p>
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <button type="submit">Login</button>
-            <p>
-              <a href="forgot-password">Forgot Password?</a>
-            </p>
-          </form>
-        )
-      )}
+      <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            name='username'
+            placeholder="Username"
+            value={loginData.username}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            name='password'
+            placeholder="Password"
+            value={loginData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Login</button>
+          <p>
+            <a href="forgot-password">Forgot Password?</a>
+          </p>
+          <p>
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+      </form>
+
     </div>
   );
 }
